0dbf216d848ec5befaa0872e67aa2656c66d3634
dzikibeduin <olszowy.patryk@gmail.com>
2023-08-07 19:56:45 +0200

    seed update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,12 +3,24 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function run() {
+    const user = await prisma.user.upsert({
+        where: { id: 1 },
+        update: {},
+        create: {
+            first_name: "John",
+            last_name: "Dog",
+            name: "jdog",
+            email: "jdog@example.com"
+        }
+    });
+
     const todo = await prisma.todo.upsert({
         where: { id: 1 },
         update: {},
         create: {
             title: "Hello",
-            description: "World"
+            description: "World",
+            authorId: user.id
         }
     });
 
