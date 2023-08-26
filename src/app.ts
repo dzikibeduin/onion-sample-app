e3b9dabf94ae758afeb7a7fd0386500abd0b8459
dzikibeduin <olszowy.patryk@gmail.com>
2023-08-26 19:45:55 +0200

    add app file - for testing

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Router } from "express";
+import dotenv from "dotenv";
+
+import { Todo } from "./data_access";
+import { TodoService } from "./services";
+import { TodoRouter } from "./routes/todo.routes";
+
+dotenv.config();
+
+const app = express();
+const router = Router();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', router);
+
+TodoRouter(router, new TodoService(new Todo));
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,4 @@
-import express, { Router } from "express";
-import dotenv from "dotenv";
-
-import { Todo } from "./data_access";
-import { TodoService } from "./services";
-import { TodoRouter } from "./routes/todo.routes";
-
-dotenv.config();
-
-const app = express();
-const router = Router();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/', router);
-
-TodoRouter(router, new TodoService(new Todo));
+import app from "./app";
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on ${process.env.PORT}`);
